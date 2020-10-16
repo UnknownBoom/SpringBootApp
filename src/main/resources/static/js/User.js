974afaf33f89b5598b4d06be1be1e857fefac9ce
UnknownBoom <artem2001sarg@mail.ru>
2020-10-16 21:02:15 +0300

    Users

---
+++ src/main/resources/static/js/User.js
@@ -0,0 +1,28 @@
+function EditUser(e) {
+    var tbody;
+    table = document.getElementById('table'); //таблица найдена
+    var i = e.parentNode.parentNode.parentElement.rowIndex; //индекс строки взят
+    console.log(i);
+    var tr = document.getElementById('table').getElementsByTagName('tr')[i];
+    document.getElementById('edit_id').value = tr.childNodes[1].textContent;
+    document.getElementById('edit_first_name').value = tr.childNodes[3].textContent;
+    document.getElementById('edit_surname').value = tr.childNodes[5].textContent;
+    document.getElementById('edit_patronymic').value = tr.childNodes[7].textContent;
+    document.getElementById('edit_username').value = tr.childNodes[9].textContent;
+}
+
+function DeleteUser(e) {
+    var tbody;
+    table = document.getElementById('table'); //таблица найдена
+    var i = e.parentNode.parentNode.parentElement.rowIndex; //индекс строки взят
+    console.log(i);
+    var tr = document.getElementById('table').getElementsByTagName('tr')[i];
+    document.getElementById('delete_id').value = tr.childNodes[1].textContent;
+}
+
+
+
+
+
+
+
